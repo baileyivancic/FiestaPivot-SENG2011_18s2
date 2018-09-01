9e13330f7e9082a24f4a1fe10a914e6a08d422d5
baileyivancic <bailey_ivancic@outlook.com>
2018-09-01 15:17:02 +1000

    All python functions working

--- python/functions.py
+++ python/functions.py
@@ -4,16 +4,16 @@ import copy
 def isSubstring(token, string):
     i = 0
     r = False
-    while (i < len(string) and  r == false):
+    while (i < len(string) and  r == False):
         m = isPrefix(token, string[i:])
         if (m == True):
             r = True
         i +=1
-    return result
+    return r
 
 def bubble(args):
     print("got in bubble")
-    
+
     listCopy = copy.copy(args)
     i = (len(args) -1)
     upperbound = 0
@@ -24,20 +24,22 @@ def bubble(args):
                 temp = listCopy[i]
                 listCopy[i] = listCopy[i - 1]
                 listCopy[i - 1] = temp
-            i+= 1
+            i-= 1
         upperbound +=1
     return listCopy
 
 def isPrefix(token, string):
     r = False
-    if len(token) == 0:
+    if (len(token) == 0):
         r = True
-    if len(string) < token:
+    if (len(string) < len(token)):
         r = False
     else:
-        r = (token == str[:len(token)])
+        r = (token == string[:len(token)])
     return r
 
-temp = bubble(sys.argv[1:])
-for p in temp:
-    print(p)
+temp = isSubstring(sys.argv[1], sys.argv[2])
+if temp == True:
+    print("yay")
+else:
+    print("no")
--- python/isPrefix.py
+++
@@ -1,9 +0,0 @@
-def isPrefix(token, string):
-    r = false
-    if len(token) == 0:
-        r = true
-    if len(string) < token:
-        r = false
-    else:
-        r = (token == str[..len(token)])
-    return r
