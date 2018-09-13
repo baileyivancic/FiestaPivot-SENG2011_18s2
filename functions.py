c2364d31fb25746bd39d54c67195d6a9ce08160d
Bailey Ivancic <bailey_ivancic@outlook.com>
2018-09-14 08:33:34 +1000

    Gitignore updated + quicksort function ported to python (not yet tested)

---
+++ functions.py
@@ -0,0 +1,74 @@
+# Temp file for all functions converted to python
+import sys
+import copy
+
+def isSubstring(token, string):
+    i = 0
+    r = False
+    while (i < len(string) and  r == False):
+        m = isPrefix(token, string[i:])
+        if (m == True):
+            r = True
+        i +=1
+    return r
+
+def bubble(args):
+    print("got in bubble")
+
+    listCopy = copy.copy(args)
+    i = (len(args) -1)
+    upperbound = 0
+    while (upperbound < len(args)):
+        i = (len(args) - 1)
+        while (i > upperbound):
+            if (listCopy[i-1] > listCopy[i]):
+                temp = listCopy[i]
+                listCopy[i] = listCopy[i - 1]
+                listCopy[i - 1] = temp
+            i-= 1
+        upperbound +=1
+    return listCopy
+
+def isPrefix(token, string):
+    r = False
+    if (len(token) == 0):
+        r = True
+    if (len(string) < len(token)):
+        r = False
+    else:
+        r = (token == string[:len(token)])
+    return r
+
+def quicksort(a=[], left, right):
+    if left < (right-1):
+        p = partition(a, left, right)
+        quicksort(a, left, p)
+        quicksort(a, p+1, right)
+
+def partition(b=[], left, right):
+    p = left
+    k = left+1
+
+    while k < right:
+        if a[k] < a[p]:
+            j = k+1
+            tmp = a[k]
+            a[k] = a[j]
+            
+            while j < p:
+                a[j+1] = a[j]
+                j+=1
+
+            a[p+1] = a[p]
+            p+=1 
+            a[p-1] = tmp
+
+        k+=1
+        
+
+
+temp = isSubstring(sys.argv[1], sys.argv[2])
+if temp == True:
+    print("yay")
+else:
+print("no")
