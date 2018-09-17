8686097ccfb76ff912d2ae15157ab0d49073cb0e
Bailey Ivancic <bailey_ivancic@outlook.com>
2018-09-18 01:06:57 +1000

    Created new databases for ads and bids. Still needs to be tested, but fairly similar to existing code. Currently working on fixing up front end templates so they are all linked up.

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import sqlite3
 
 class Database(object):
 
+#   Common functions
     def __init__(self):
         db = self.get_db()
 
@@ -22,6 +23,7 @@ class Database(object):
     def first_run(self):
         pass
 
+# Login db functions
     def register_user(self, username, password, email, city, state):
         db = self.get_db()
         cursor = db.cursor()
@@ -40,8 +42,6 @@ class Database(object):
             db.commit()
             db.close()
             return 1
-
-
 
     def isValidUser(self, email, password):
         db = self.get_db()
@@ -74,7 +74,185 @@ class Database(object):
             # there is already a user using this user name
             isValid = True
 
-
         self.close(db)
-
         return isValid
+
+# Advertisement db functions
+    #UNTESTED
+    def create_ad(userID, title, price, area, descr, active):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM ads WHERE userID=?)", (userID))
+        temp = cursor.fetchone
+
+        if temp == 0:
+            # Could not find a user associated with this userID
+            print("Something went wrong, could not access correct user column\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO ads (userID, title, price, area, descr, active) VALUES (?, ?, ?, ?, ?)''',(userID, title, price, area, descr, active))
+            return 1
+    
+    #UNTESTED
+    def updateTitle_ad(adID, newTitle):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM ads WHERE adID=?)", (adID))
+        temp =  cursor.fetchone()
+        
+        if temp == 0:
+            # Could not find ad assocoated with this adID
+            print("Something went wrong, ad does not exist\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO ads (title) VALUES (?)''',(newTitle))
+            db.commit()
+            db.close()
+            return 1
+
+    #UNTESTED
+    def updatePrice_ad(adID, newPrice):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM ads WHERE adID=?)", (adID))
+        temp =  cursor.fetchone()
+        
+        if temp == 0:
+            # Could not find ad assocoated with this adID
+            print("Something went wrong, ad does not exist\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO ads (price) VALUES (?)''',(newPrice))
+            db.commit()
+            db.close()
+            return 1
+
+    #UNTESTED
+    def updateLocation_ad(adID, newLocation):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM ads WHERE adID=?)", (adID))
+        temp =  cursor.fetchone()
+        
+        if temp == 0:
+            # Could not find ad assocoated with this adID
+            print("Something went wrong, ad does not exist\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO ads (location) VALUES (?)''',(newLocation))
+            db.commit()
+            db.close()
+            return 1
+
+    #UNTESTED
+    def updateDescription_ad(adID, newDesc):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM ads WHERE adID=?)", (adID))
+        temp =  cursor.fetchone()
+        
+        if temp == 0:
+            # Could not find ad assocoated with this adID
+            print("Something went wrong, ad does not exist\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO ads (description) VALUES (?)''',(newDesc))
+            db.commit()
+            db.close()
+            return 1
+    
+    #UNTESTED
+    def updateActive_ad(adID, newActive):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM ads WHERE adID=?)", (adID))
+        temp =  cursor.fetchone()
+        
+        if temp == 0:
+            # Could not find ad assocoated with this adID
+            print("Something went wrong, ad does not exist\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO ads (active) VALUES (?)''',(newActive))
+            db.commit()
+            db.close()
+            return 1
+
+# Bid db functions
+    #UNTESTED
+    def create_bid(adID, userID, price, comment):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM bids WHERE adID=? AND userID=?)", (adID, userID))
+        temp = cursor.fetchone
+
+        if temp == ( ,0) or temp == (0, ): #CHECK THIS
+            # Could not find userID or adID
+            print("Something went wrong, could not access correct user or ad\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO bids (adID, userID, price, comment) VALUES (?, ?, ?, ?)''',(adID, userID, price, comment))
+            db.commit()
+            db.close()
+            return 1
+    
+    #UNTESTED
+    def updatePrice_bid(adID, userID, newPrice):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM bids WHERE adID=? AND userID=?)", (adID, userID))
+        temp =  cursor.fetchone()
+        
+        if temp == ( ,0) or temp == (0, ): #CHECK THIS
+            # Could not find userID or adID
+            print("Something went wrong, could not access correct user or ad\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO bids (price) VALUES (?)''',(newPrice))
+            db.commit()
+            db.close()
+            return 1
+
+    #UNTESTED
+    def updateComment_bid(adID, userID, newComment):
+        db = self.get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM bids WHERE adID=? AND userID=?)", (adID, userID))
+        temp =  cursor.fetchone()
+        
+        if temp == ( ,0) or temp == (0, ): #CHECK THIS
+            # Could not find userID or adID
+            print("Something went wrong, could not access correct user or ad\n")
+            db.commit()
+            db.close()
+            return 0
+        else :
+            cursor.execute('''INSERT INTO bids (comment) VALUES (?)''',(newComment))
+            db.commit()
+            db.close()
+            return 1
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS accounts(
-    ID          INTEGER    PRIMARY KEY AUTOINCREMENT,
+    ID          INTEGER     PRIMARY KEY AUTOINCREMENT,
     username    TEXT        NOT NULL,
     password    TEXT        NOT NULL,
     email       TEXT        NOT NULL,
@@ -8,3 +8,23 @@ CREATE TABLE IF NOT EXISTS accounts(
     STATE       TEXT        NOT NULL,
     RATING      DECIMAL
 );  
+
+/* Add in extra criteria that we want to incude in the ad element */
+CREATE TABLE IF NOT EXISTS ads(
+    ID          INTEGER     PRIMARY KEY AUTOINCREMENT,
+    userID      INTEGER     FOREIGN KEY,
+    title       TEXT        NOT NULL,
+    price       DECIMAL     NOT NULL,
+    area        TEXT        NOT NULL,
+    descr       TEXT        NOT NULL,
+    active      BOOLEAN     NOT NULL
+);
+
+/* Add in extra criteria we want to include*/
+CREATE TABLE IF NOT EXISTS bids(
+    ID          INTEGER     PRIMARY KEY AUTOINCREMENT,
+    adID        INTEGER     FOREIGN KEY,
+    userID      INTEGER     FOREIGN KEY,
+    price       DECIMAL     NOT NULL,
+    comment     TEXT
+);
